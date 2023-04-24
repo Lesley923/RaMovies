305457e94704c966aa05054386f03fea4e01195f
Jimei Fang <fjmabc00@126.com>
2023-04-23 21:26:52 -0400

    security

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const bodyParser = require('body-parser');
 
 const cookieParser = require('cookie-parser');
@@ -14,27 +19,49 @@ const app = express();
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// serving static files
-app.use(express.static(path.join(__dirname, 'public')));
-
 //middelware
+//set security HTTP headers
+app.use(helmet());
+
+//Development logging
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windows: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/v1', limiter);
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Data sanitization against XSS
+app.use(xss());
+
+//prevent parameters pollution
+app.use(hpp());
+// app.use(hpp({
+//   whitelist:['']
+// }));
+
+//Body parser.reading data from body into req.body
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 // Body parser, reading data from body into req.body
 app.use(cookieParser());
-
 
 // Test middleware
 app.use((req, res, next) => {
@@ -43,7 +70,8 @@ app.use((req, res, next) => {
   next();
 });
 
-
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 //router
 app.use('/', viewRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (obj, ...allowFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -11,6 +20,39 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //create error if user post password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError('this route is not used for upodate password!', 400)
+    );
+  }
+  //filter fileds which can be updated
+  const filterBody = filterObj(req.body, 'username', 'email');
+  //update user document
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
