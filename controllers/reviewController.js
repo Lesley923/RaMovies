a350978aebeb547b12c8dd3aefc5de2c45ec27f6
Jimei Fang <fjmabc00@126.com>
2023-04-25 20:37:31 -0400

    detail movie

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,15 +1,33 @@
+const { async } = require('q');
 const Review = require('./../models/reviewModel');
 const AppError = require('./../utils/appError');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
+const Movie = require('./../models/movieModel');
 
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.movie_id) req.body.movie_id = req.params.movieId;
   if (!req.body.user_id) req.body.user_id = req.user.id;
   next();
 };
+exports.addUserAndMovieInfo = (req, res, next) => {
+  req.body.user_id = req.user.id;
+  req.body.movie_id = req.params.id;
+  next();
+};
 exports.getAllReviews = factory.getAll(Review, 'admin_reviews', 'Manage');
 exports.getReview = factory.getOne(Review, null, 'detail_reviews', 'Detail');
-exports.createReview = factory.createOne(Review);
+exports.createReview = catchAsync(async (req, res, next) => {
+  await Review.create(req.body);
+  const doc = await Movie.findById(req.params.id).populate({
+    path: 'reviews',
+    fields: 'content rating user_id',
+  });
+
+  res.status(200).render('movie', {
+    title: 'movie detail',
+    doc,
+  });
+});
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review, 'admin_reviews', 'Manage');
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
+const reviewController = require('./../controllers/reviewController');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
@@ -8,8 +9,17 @@ router.use(authController.isLoggedIn);
 
 router.get('/', viewsController.getOverview);
 
-router.get('/movie/:slug', viewsController.getMovie);
+router.get('/movie/:slug', authController.protect, viewsController.getMovie);
 
 router.get('/login', viewsController.getLoginForm);
+
+router
+  .route('/review/:id')
+  .get(viewsController.showAddReviewForm)
+  .post(
+    authController.protect,
+    reviewController.addUserAndMovieInfo,
+    reviewController.createReview
+  );
 
 module.exports = router;
