f676a5a5dafd51803f72c73d940a692094448a05
Jimei Fang <fjmabc00@126.com>
2023-04-27 02:09:15 -0400

    new signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,13 +29,23 @@ const createSendToken = (user, statusCode, res) => {
 
   user.password = undefined;
   // console.log(token);
+  if (statusCode === 201) {
+    res.redirect('/');
+  } else {
+    res.status(statusCode).json({
+      status: 'success',
+      token,
+      data: {
+        user,
+      },
+    });
+  }
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
   console.log(req.body);
   const newUser = await User.create(req.body);
   createSendToken(newUser, 201, res);
-  res.redirect('/');
 });
 
 exports.login = catchAsync(async (req, res, next) => {
